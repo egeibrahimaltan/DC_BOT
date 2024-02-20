ca5ebc4f1a60f7272577a9a870c0d860edf99a35
Ege İbrahim Altan <134962603+egeibrahimaltan@users.noreply.github.com>
2024-02-20 21:49:57 +0300

    Update ayarlar.py

--- ayarlar.py
+++ ayarlar.py
@@ -2,5 +2,5 @@
 
 ayarlar = {
     "on_taki": ">", #ön takı, prefix (maybe it won't work if we call it different here)
-    "TOKEN": "MTIwNTkxOTE0NjQ0NjE1OTk5NA.Gw2W1C.XaHpdJxHB1or_eD2MOdeK28dcTOedv6wFOKhi8"
+    "TOKEN": "TOKEN"
 }
